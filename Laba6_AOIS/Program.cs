fd17e00ccc88ef7da8f101ceb838899ddf21334b
Danshicu <Promchuk.danik@gmail.com>
2023-05-11 08:56:19 +0300

    ready to show

--- Laba6_AOIS/Program.cs
+++ Laba6_AOIS/Program.cs
@@ -1,22 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Laba6_AOIS;
 
-using Laba6_AOIS;
-
-Console.WriteLine("\nHello, World!");
 HashTable table = new HashTable(30);
-table.AddNode("ab", "some");
-table.AddNode("ab", "interesting");
-table.AddNode("not", "collision");
-table.AddNode("ab", "info");
-table.FindNode("not");
+table.AddNode("power", "the amount of energy transferred per unit time");
+table.AddNode("power", "the amount of energy converted per unit time");
+table.AddNode("Power", "the rate at which electrical energy is transferred");
+table.AddNode("watt", "the unit of power");
 table.ShowTable();
+table.FindNode("nothing");
 table.DeleteNode("not");
+table.AddNode("force","influence that causes the motion of an object with mass to change its velocity");
+table.DeleteNode("Power");
 table.ShowTable();
-table.AddNode("SOMEINFO","XZ");
-table.DeleteNode("ab");
-table.DeleteNode("th");
-table.FindNode("SOMEINFO");
-table.FindNode("XZ");
-table.AddNode("SOMEINFO", "NEW");
-table.AddNode("SOMEINFO", "INFO");
+table.FindNode("power");
+table.AddNode("velocity", "the directional speed of an object in motion");
+table.AddNode("speed", "quantity that refers to how fast an object is moving");
 table.ShowTable();
+table.FindNode("velocity");
